de636a39a96310cb0c3b15a92e5778c0ca7d24bc
AnhVaccari <anh.vaccari@orange.fr>
2025-08-07 14:46:42 +0200

    refactor: remove unused CommentController

--- back/src/main/java/com/openclassrooms/mddapi/controller/AuthController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/AuthController.java
@@ -2,7 +2,6 @@ package com.openclassrooms.mddapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
