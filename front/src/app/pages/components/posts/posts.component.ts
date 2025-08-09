e5e1a2f49a12497c45cdafa24161a696d0e72ecd
AnhVaccari <anh.vaccari@orange.fr>
2025-08-09 19:15:49 +0200

    style: update styling for create-post page

--- front/src/app/pages/components/posts/posts.component.ts
+++ front/src/app/pages/components/posts/posts.component.ts
@@ -10,24 +10,24 @@ export class PostsComponent implements OnInit {
 
   posts = [
     {
-      name: 'Titre du thème 1',
+      name: "Titre de l'article",
 
       description:
         'Alii summum decus in carruchis solito altioribus et ambitioso vestium cultu ponentes sudant sub ponderibus lacernarum, quas in collis insertas cingulis ipsis adnectunt nimia subtegminum tenuitate perflabiles, expandentes eas crebris agitationibus maximeque sinistra, ut longiores fimbriae tunicaeque perspicue luceant varietate liciorum effigiatae in species animalium multiformes.',
     },
     {
-      name: 'Titre du thème 2',
+      name: "Titre de l'article",
       description:
         'Alii summum decus in carruchis solito altioribus et ambitioso vestium cultu ponentes sudant sub ponderibus lacernarum, quas in collis insertas cingulis ipsis adnectunt nimia subtegminum tenuitate perflabiles, expandentes eas crebris agitationibus maximeque sinistra, ut longiores fimbriae tunicaeque perspicue luceant varietate liciorum effigiatae in species animalium multiformes.',
     },
     {
-      name: 'Titre du thème 3',
+      name: "Titre de l'article",
 
       description:
         'Alii summum decus in carruchis solito altioribus et ambitioso vestium cultu ponentes sudant sub ponderibus lacernarum, quas in collis insertas cingulis ipsis adnectunt nimia subtegminum tenuitate perflabiles, expandentes eas crebris agitationibus maximeque sinistra, ut longiores fimbriae tunicaeque perspicue luceant varietate liciorum effigiatae in species animalium multiformes.',
     },
     {
-      name: 'Titre du thème 5',
+      name: "Titre de l'article",
 
       description:
         'Alii summum decus in carruchis solito altioribus et ambitioso vestium cultu ponentes sudant sub ponderibus lacernarum, quas in collis insertas cingulis ipsis adnectunt nimia subtegminum tenuitate perflabiles, expandentes eas crebris agitationibus maximeque sinistra, ut longiores fimbriae tunicaeque perspicue luceant varietate liciorum effigiatae in species animalium multiformes.',
