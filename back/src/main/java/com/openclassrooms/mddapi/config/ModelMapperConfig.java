b803172f12882db7b2b271872c8810d95ef8313c
AnhVaccari <anh.vaccari@orange.fr>
2025-08-11 17:09:14 +0200

    feat: add ModelMapper configuration to properly map User and Topic relations

--- back/src/main/java/com/openclassrooms/mddapi/config/ModelMapperConfig.java
+++ back/src/main/java/com/openclassrooms/mddapi/config/ModelMapperConfig.java
@@ -4,11 +4,20 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.openclassrooms.mddapi.dto.PostResponse;
+import com.openclassrooms.mddapi.model.Post;
+
 @Configuration
 public class ModelMapperConfig {
     @Bean
     public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
+        ModelMapper mapper = new ModelMapper();
 
+        // Configuration pour Post → PostResponse
+        mapper.createTypeMap(Post.class, PostResponse.class)
+                .addMapping(src -> src.getUser().getUsername(), PostResponse::setAuthorUsername)
+                .addMapping(src -> src.getTopic().getName(), PostResponse::setTopicName);
+
+        return mapper;
+    }
 }
